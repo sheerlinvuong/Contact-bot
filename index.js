2243412e03b0406104ff237fa7563fa8849a4126
sheerlinvuong <sheerlinvuong@gmail.com>
2018-07-26 17:01:48 +0100

    added recaptcha for contact form

--- index.js
+++ index.js
@@ -6,18 +6,51 @@ const telegram = new Telegram(process.env.BOT_TOKEN, {
   agent: null, // https.Agent instance, allows custom proxy, certificate, keep alive, etc.
   webhookReply: true,
 });
+const fetch = require('node-fetch');
+
 app.use(express.json());
 
-app.post('/contact', (req, res) => {
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
+app.post('/contact', async (req, res) => {
+  if (!req.body.gToken || req.body.gToken === '') {
+    res.status(400);
+    return res.send({ message: 'Missing reCaptcha token' });
+  }
+  const { URLSearchParams } = require('url');
+  const url = 'https://www.google.com/recaptcha/api/siteverify';
+
+  const params = new URLSearchParams();
+  params.append('secret', process.env.SECRET_KEY);
+  params.append('response', req.body.gToken);
+  const response = await fetch(url, {
+    method: 'POST',
+    body: params,
+  }).then(res => res.json());
+
+  if (!response.success) {
+    res.status(400);
+    return res.send({ message: 'Recaptcha Invalid' });
+  }
+
   telegram.sendMessage(
     process.env.SHEERLIN_ID,
-    ` name: ${req.body.name}
-    subject: ${req.body.subject}
-  contact: ${req.body.contact}
-  message: ${req.body.message}`,
+    `
+    name: ${req.body.name}
+    email: ${req.body.email}
+    message: ${req.body.message}
+    `,
   );
-
   res.send('ok');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
+
+//subject: ${req.body.subject}
